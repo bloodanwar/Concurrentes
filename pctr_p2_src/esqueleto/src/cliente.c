c60fc1a75d9183b2b44b6b3d1ec968fbda16ab57
Carlos Rincón <bloodanwar@gmail.com>
2022-03-10 19:22:51 +0100

    init

---
+++ pctr_p2_src/esqueleto/src/cliente.c
@@ -0,0 +1,35 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]){
+    
+    /*
+    
+    wait(mutex);
+        if num_clientes == N then
+            signal(mutex);      
+            return;
+        num_clientes++;
+    signal(mutex)
+    
+    wait(sillón);
+        wait(mutex);
+            num_clientes--;
+        signal(mutex);
+        signal(barbero);
+        wait(fin);
+    signal(sillón);
+
+    */
+
+
+    printf("Soy el cliente %d",getpid());
+
+    return EXIT_SUCCESS;
+}
